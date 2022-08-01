2972439b206ea320a19a95b131945d5d4e66448b
Andre Nel <acnel@s-plane.com>
2022-08-01 10:53:25 +0200

    another commit

--- analyser.sh
+++ analyser.sh
@@ -3,7 +3,8 @@
 for path in $(find -name "*.txt"); do
     if [[ "$path" = *.txt ]]; then
         lines=$(wc -l < "${path:2}")
-        echo "$lines"
+        echo "lines = $lines"
+        echo "git log --oneline | wc -l = $(git log --oneline | wc -l)"
     else 
         echo "...."
     fi
